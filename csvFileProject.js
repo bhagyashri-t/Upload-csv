1a79c0f6f438c1e09cc599afb0bfae8cca7c96cd
Bhagyashri Tamkhane <bhagyashri.tamkhane@gmail.com>
2015-05-29 16:47:07 -0700

    Commented code

--- csvFileProject.js
+++ csvFileProject.js
@@ -5,25 +5,33 @@ $(document).ready(function() {
 	$('#showUsersBtn').bind('click', showAllUsers);
 });
 
+/*
+	showAllUsers() :
+		This function gets all the users from server and displys them in table.
+*/
+
 function showAllUsers() {
 	
 	var output = '';
 	var headers = ['first_name', 'last_name', 'add_line_1', 'add_line_2', 'city', 'state', 'postal_code'];
 
+	//Get request to get all the users in the database
 	$.get('/csvData', function(data, success){
-		console.log(data);		
+//		console.log(data);		
 
 		if(data.length > 0) {
 
 			output += '<table border>\r\n';
 			output += '<tr>\r\n';
 
+			//Adding header row
 			for (var j = 0; j < headers.length; j++) {
 				output += '<td>'+ headers[j] + '</td>';
 			};
 
 			output += '</tr>\r\n';
 
+			//Adding response data to the table
 			for (var i = 0; i < data.length; i++) {
 				output += '<tr>\r\n';
 				for(value in data[i]) {
@@ -38,11 +46,16 @@ function showAllUsers() {
 			output += 'No records exists in database.';
 		}
 			
-		console.log(output);
+//		console.log(output);
 
 		$('#csvResult').after(output);
 	});
 }
+
+/*
+	uploadCSVFile(event) :
+		This function opens the csv file and saves the content in the database.
+*/
 
 function uploadCSVFile(event) {
 
@@ -57,32 +70,30 @@ function uploadCSVFile(event) {
 
 	reader.onload = function(event) {
 		var csv = event.target.result;
-		console.log(csv);
+
 		var data = $.csv.toArrays(csv);
 		var td;
-		console.log(data);
+//		console.log(data);
 		
 		for(row in data) {
 			output += '<tr>\n';
 			for(item in data[row]) {
 				td = data[row][item].charAt(0).toUpperCase() + data[row][item].slice(1);
-//				td = data[row][item];
 				output += '<td>' + td + '</td>';				
 			}
 			output += '</tr>';
 		}
-		output += '</table><br>';
-
-		console.log(output);
-		
+		output += '</table><br>';		
 
 		var jsonCsvData = {array: parseCSVToObject(csv)};
-		console.log(jsonCsvData);
-
+//		console.log(jsonCsvData);
+		
+		//Post request to server to save csv data to database
 		$.post('/csvData', jsonCsvData, function(data, success) {
-			console.log("Data: "+data+ "success: "+ success);
+
 			output += '<p>Saved data from uploaded csv file</p>';
 			$('#csvResult').html(output);
+
 		});
 		
 	}	
@@ -91,45 +102,41 @@ function uploadCSVFile(event) {
 
 /* 
 
-parseCSVToObject() :- 
-
-	Description : 
-	 	This function accepts csv file as parameter and parse it into a json object
-	 	for further manipulation.
+parseCSVToObject() :-  
+	 	This function accepts csv content as parameter and parse it into a json object
+	 	and return json.
 */
 
+function parseCSVToObject (csvContent) {
 
 
-function parseCSVToObject (csvFile) {
-
-
-	var fileLines = csvFile.split('\n');
-	console.log(fileLines[fileLines.length-1] == '');
+	var fileLines = csvContent.split('\n');
 
 	if(fileLines[fileLines.length-1] == '') {
 		fileLines.splice(fileLines.length-1, 1);
 	}
-	console.log(fileLines.length);
 
 	var csvFirstLine = fileLines[0].split(',');
-	console.log(csvFirstLine);
 
 	var csvHeaders = ['first_name', 'last_name', 'add_line_1', 'add_line_2', 'city', 'state', 'postal_code'];
 	var i, j, lineCount;
 	var result = [];
 
+	//checking if header is present in input csvContent
 	for (var header = 0; header < csvHeaders.length; header++) {
 		if(csvFirstLine[header] == csvHeaders[header]) {
+			
 			lineCount = 1;
-			console.log(lineCount);
 			continue;
+
 		} else {
-			lineCount = 0;
-			console.log(lineCount);
+			
+			lineCount = 0;			
 			break;
 		}
 	}
 
+	//creating array objects for all the csv lines
 	for (i = lineCount; i < fileLines.length; i++) {
 		var object = {}, currentLine;
 
@@ -137,7 +144,6 @@ function parseCSVToObject (csvFile) {
 		
 		for (j = 0; j < csvHeaders.length; j++) {
 			object[csvHeaders[j]] = currentLine[j].charAt(0).toUpperCase() + currentLine[j].slice(1);
-//			object[csvHeaders[j]] = currentLine[j];
 		};
 
 		result.push(object);
--- csvServer.js
+++ csvServer.js
@@ -1,19 +1,46 @@
 
+// Module dependancies
 var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 
 var app = express();
 
-var mysqlConnection = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'csvData'
-});
+//Server port and IP address, working for both locally and openshift server.
+var serverPort = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.listen(8000, 'localhost', function(){
-	console.log('server running on lcalhost:8000');
+//Mysql connect configuration, working locally and on the server.
+var mysqlHost = process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost';
+var mysqlPort = process.env.OPENSHIFT_MYSQL_DB_PORT || '3306';
+
+//Connecting to mysql database (local/Server)
+if(mysqlHost == 'localhost') {
+	mysqlConnection = mysql.createConnection ({
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'csvData'
+	});
+	console.log('Connected to local database csvData');
+} else {
+	var mysqlUser = 'adminQA6hpVU' ;
+	var mysqlPassword = 'bGSIQQx2SjVr';
+	var mysqlDb = 'uploadcsv';
+	var mysqlString = 'mysql://' + mysqlUser + ':' + mysqlPassword + '@' + mysqlHost + ':' + mysqlPort + '/' + mysqlDb;
+	
+	mysqlConnection = mysql.createConnection(mysqlString);
+	mysqlConnection.connect(function(error) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Connected to server database uploadcsv');
+		}
+	});
+}
+
+app.listen(serverPort, serverIpAddress, function () {
+	console.log("Listening on "+ serverIpAddress +", port "+ serverPort);
 });
 
 app.use(express.static(__dirname));
@@ -22,43 +49,24 @@ console.log(__dirname);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//Rendering app on browser
 app.get('/', function(request, response) {
 	response.send(index.html);
 	console.log("showing csvParser.html");
 });
 
+//API to post csv data
 app.post('/csvData', function(request, response) {
-
-	console.log(request.body.array);
-//	console.log(JSON.parse(request.body.array));
 
 	var rowsToadd = JSON.parse(request.body.array);
 	var row;
-//	console.log(rowsToadd);
-/*
-	var i = 0;
-	row = {first_name: rowsToadd[i].first_name,
-				last_name: rowsToadd[i].last_name,
-				add_line1: rowsToadd[i].add_line_1,
-				add_line2: rowsToadd[i].add_line_2,
-				city: rowsToadd[i].city,
-				state: rowsToadd[i].state,
-				postal_code: rowsToadd[i].postal_code};
-	console.log(row);
-	console.log(row.first_name);
-	console.log(row.last_name);
-*/
 	
 	function sqlCallback (i) {
 		return function (error, rows, fields) {
 			
 				if(error) {
 					console.log(error);
-		/*				response.json({
-						type: false,
-						data: "Error :"+ error
-					});
-		*/				
+				
 				} else if(rows.length <= 0) {
 					row = {first_name: rowsToadd[i].first_name,
 						last_name: rowsToadd[i].last_name,
@@ -72,20 +80,15 @@ app.post('/csvData', function(request, response) {
 					mysqlConnection.query('INSERT INTO users SET ?', row, function(error, rows, fields) {
 						if(error) {
 							console.log(error);
-
-		/*						response.json({
-								type: false,
-								data: "Error: "+ error
-							});
-		*/					
+					
 						} else {
 							console.log("User Added successfully!");
 							console.log(rows);
-		//						response.send("400");
+
 						}
 					});
 				} else {
-		//				response.send("404");
+
 					console.log("User already exists!");
 				}
 			}	
@@ -97,10 +100,11 @@ app.post('/csvData', function(request, response) {
 		mysqlConnection.query(sqlQuery, sqlCallback(i));
 	};
 	
-	response.send("404");
+	response.send("400");
 
 });
 
+//API to get existing csv data
 app.get('/csvData', function(request,response) {
 
 	var sqlQuery = 'SELECT first_name, last_name, add_line1, add_line2, city, state, postal_code FROM users';
